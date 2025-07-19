ed2c67971c04007723b44845121d9768bfdc115f
Brandon Kindred <brandon.kindred@gmail.com>
2025-07-19 00:41:39 -0400

    fix: core upgrade

--- src/main/java/com/looksee/contentAudit/Application.java
+++ src/main/java/com/looksee/contentAudit/Application.java
@@ -4,20 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.looksee*"})
+@SpringBootApplication(scanBasePackages = {"com.looksee.contentAudit"})
 @PropertySources({
 	@PropertySource("classpath:application.properties")
 })
-@EnableNeo4jRepositories("com.looksee.contentAudit.models.repository")
-@EntityScan(basePackages = { "com.looksee.contentAudit.models"} )
 public class Application {
 	@SuppressWarnings("unused")
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
